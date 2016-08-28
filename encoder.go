e988bf3c049bdd56abd3dff4b469684f593a2e6c
Brian Rackle <brianrackle@hotmail.com>
2016-08-28 15:10:25 -0700

    cleanup

--- encoder.go
+++ encoder.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os/exec"
@@ -20,21 +19,19 @@ func main() {
 	}
 
 	for _, file := range files {
-		sourceFile := filepath.Join(sourceDir, file.Name())
-		targetFile := filepath.Join(targetDir, file.Name())
-		command := fmt.Sprintf(baseCommand, sourceFile, targetFile)
-		if len(command) != 0 {
-			cmd := exec.Command(baseCommand, "-i", sourceFile, "-t", "1", "-o", targetFile, "-f", "mp4", "-O", "-X", "1920", "--loose-anamorphic",
-				"--modulus", "2", "-e", "x264", "-q", "19", "--vfr", "-a", "1", "-E", "av_aac", "-6", "dpl2", "-R", "Auto", "-B", "160", "--audio-fallback",
-				"ac3", "--encoder-preset", "slower", "--encoder-tune", "film", "--encoder-level", "4.0", "--encoder-profile", "high", "--verbose", "1")
-			println(strings.Join(cmd.Args, " "))
+		fileName := file.Name()
+		sourceFile := filepath.Join(sourceDir, fileName)
+		targetFile := filepath.Join(targetDir, fileName[0:len(fileName)-len(filepath.Ext(fileName))]+".mp4")
+		cmd := exec.Command(baseCommand, "-i", sourceFile, "-t", "1", "-o", targetFile, "-f", "mp4", "-O", "-X", "1920", "--loose-anamorphic",
+			"--modulus", "2", "-e", "x264", "-q", "19", "--vfr", "-a", "1", "-E", "av_aac", "-6", "dpl2", "-R", "Auto", "-B", "160", "--audio-fallback",
+			"ac3", "--encoder-preset", "slower", "--encoder-tune", "film", "--encoder-level", "4.0", "--encoder-profile", "high", "--verbose", "1")
+		println(strings.Join(cmd.Args, " "))
 
-			err = cmd.Run()
+		err = cmd.Run()
 
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
+		if err != nil {
+			log.Fatal(err)
+			return
 		}
 	}
 }
